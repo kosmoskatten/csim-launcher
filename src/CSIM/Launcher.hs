962df019cae827d442dc2b23d26922e6089524be
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-11-20 14:43:01 +0100

    Clean up.

--- src/CSIM/Launcher.hs
+++ src/CSIM/Launcher.hs
@@ -22,59 +22,70 @@ import           Data.List                        (nubBy)
 import           Data.Text                        (Text, unpack)
 import           Text.Printf                      (printf)
 
+-- | Read a 'StartSpecification' from the provided file path.
 startSpecificationFromFile :: FilePath -> IO (Either String StartSpecification)
 startSpecificationFromFile file = eitherDecode <$> LBS.readFile file
 
+-- | Read a 'SystemDefinition' from the provided file path.
 systemDefinitionFromFile :: FilePath -> IO (Either String SystemDefinition)
 systemDefinitionFromFile file = eitherDecode <$> LBS.readFile file
 
+-- | Calculate a set of 'Component's to start from the 'SystemDefinition'
+-- in order to satisify the 'StartSpecification'.
 calculateStartSet :: SystemDefinition -> StartSpecification
                   -> Either String [Component]
-calculateStartSet sys start =
-    expandRequirements sys =<< findCompatibleComponents sys start
+calculateStartSet sys start = expandReqs sys =<< findComponents sys start
 
 -- | From the 'StartSpecification' and the 'SystemDefinition' find a set of
 -- 'Component's that is matching the requirements in the 'StartSpecification'.
-findCompatibleComponents :: SystemDefinition -> StartSpecification
-                         -> Either String [Component]
-findCompatibleComponents sys start =
-    findCompatibleComponents' (components sys) $ HM.toList $ requires start
+findComponents :: SystemDefinition -> StartSpecification -> Either String [Component]
+findComponents sys = findComponents' (components sys) . HM.toList . requires
 
-findCompatibleComponents' :: [Component] -> [(Text, Range)] -> Either String [Component]
-findCompatibleComponents' cs rs = unique <$> mapM (findCompatibleComponent cs) rs
+-- | Really the implementation of 'findComponents'', but using simpler types.
+-- The list have only unique components, with regards to their names.
+findComponents' :: [Component] -> [(Text, Range)] -> Either String [Component]
+findComponents' cs rs = unique <$> mapM (findComponent cs) rs
 
-findCompatibleComponent :: [Component] -> (Text, Range)
-                        -> Either String Component
-findCompatibleComponent [] (service, _)           =
+-- | Find a 'Component' that's providing the given service name and version.
+findComponent :: [Component] -> (Text, Range) -> Either String Component
+
+-- | No more components to search. Error.
+findComponent [] (service, _)           =
     Left $ printf "No component provides service '%s'" (unpack service)
-findCompatibleComponent (x:xs) s@(service, range) =
-    case HM.lookup service (provides x) of
+
+-- | Examine the 'Component' c. If c is providing a service matching the
+-- contstrains then the search is done. If a provided service not is matching
+-- with regards to versioning there's an error.
+findComponent (c:cs) s@(service, range) =
+    case HM.lookup service (provides c) of
         Just version'
-            | version' `inRange` range -> Right x
+            | version' `inRange` range -> Right c
             | otherwise                ->
                 Left $
                     printf "Incompatible service found for '%s'. Requires '%s', but provided is '%s'"
                            (unpack service) (show range) (show version')
-        Nothing      -> findCompatibleComponent xs s
+        Nothing      -> findComponent cs s
 
-expandRequirements :: SystemDefinition -> [Component] -> Either String [Component]
-expandRequirements sys = go 0
+-- | Recursively expand the requirements for the components. Expand until
+-- the list of required components no longer is growing.
+expandReqs :: SystemDefinition -> [Component] -> Either String [Component]
+expandReqs sys = go 0
   where
     go :: Int -> [Component] -> Either String [Component]
     go n cs = do
-        cs' <- doExpansion cs
+        cs' <- snd <$> foldM expandReq (components sys, cs) cs
         let lcs = length cs'
         if lcs > n
             then go lcs cs'
             else Right cs'
 
-    doExpansion :: [Component] -> Either String [Component]
-    doExpansion cs = snd <$> foldM expandRequirement (components sys, cs) cs
-
-expandRequirement :: ([Component], [Component]) -> Component -> Either String ([Component], [Component])
-expandRequirement (sys, selected) component = do
-    selected' <- findCompatibleComponents' sys (HM.toList $ dependsOn component)
+-- | Expand the requirements for the 'Component' component.
+expandReq :: ([Component], [Component]) -> Component
+          -> Either String ([Component], [Component])
+expandReq (sys, selected) component = do
+    selected' <- findComponents' sys (HM.toList $ dependsOn component)
     Right (sys, unique $ selected ++ selected')
 
+-- | Make a list unique with regards to the names of the components.
 unique :: [Component] -> [Component]
 unique = nubBy (\x y -> name x == name y)
